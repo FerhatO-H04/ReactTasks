72b68164d87071031e7364bd6aa74e42b2c4b1ed
FerhatO-H04 <131246514+FerhatO-H04@users.noreply.github.com>
2023-10-03 23:24:51 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+
+export function App(){
+  return(
+    <> 
+      <header className="header">
+        <h1>Header Section</h1>
+      </header>
+      <main>
+        <section>
+          <h3>SectionOne</h3>
+        </section>
+        <section>
+          <h3>SectionTwo</h3>
+        </section>
+      </main>
+      <footer>
+        <h4>Footer</h4>
+      </footer>
+    </>
+  )
+} 
